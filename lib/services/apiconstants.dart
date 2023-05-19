5d63f1f36a61d7bfcf9d34d47583fa647b91f4a4
Deepak <deepak.cris2020@gmail.com>
2023-05-19 12:06:19 +0530

    changes in api key

--- lib/services/apiconstants.dart
+++ lib/services/apiconstants.dart
@@ -1,2 +1,2 @@
 const String BASEURI = 'https://api.openai.com/v1/models';
-const String KEY = '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';
+const String KEY = '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';
